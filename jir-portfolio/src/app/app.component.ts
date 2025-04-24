506e28c549bd764b3a6f2ae92a2c41df1899d1fa
Javier Iranzo <jiranzo@iti.es>
2025-04-24 17:29:32 +0200

    Config project

--- jir-portfolio/src/app/app.component.ts
+++ jir-portfolio/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    WelcomePageComponent,
+    HomePageComponent,
+    NavMenuComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- jir-portfolio/src/app/app.routes.ts
+++ jir-portfolio/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'welcome', component: WelcomePageComponent },
+    { path: 'home', component: HomePageComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+    { path: '**', redirectTo: '/home' } 
+    
+];
